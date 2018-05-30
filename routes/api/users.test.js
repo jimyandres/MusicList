6e7f9fd7731ff24c1a2f068b6a1648fe8990d8e6
jimyandres <jimyandres@utp.edu.co>
2018-05-29 20:42:31 -0500

    typos fixed

--- routes/api/users.test.js
+++ routes/api/users.test.js
@@ -10,15 +10,15 @@ describe('The User API', () => {
 		// Connect to the server and get a response
 		// Expect that response to be 200 and and server JSON
 		const res = await request('http://localhost:3000')
-			.get('/api/user/list')
+			.get('/api/users/list')
 			.expect(200)
-			.expect('Content-Type', '/json/');
+			.expect('Content-Type', 'application/json; charset=utf-8');
 
 		// These expects are jest, not supertest
 		// First, expect to get a result that is an array
 		expect(Array.isArray(res.body)).toBe(true);
 		// Second, expect the array to have something in it
-		expect(res.body.lenght).toBeGreaterThan(0);
+		expect(res.body.length).toBeGreaterThan(0);
 		// Third, expect the username of the first returned user to be Administrator
 		expect(res.body[0].username).toBe('admin');
 	});
