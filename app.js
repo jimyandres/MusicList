a8ba90d93d425c0eac8f50797fc0a1e0db2b712b
jimyandres <jimyandres@utp.edu.co>
2018-05-31 20:07:22 -0500

    eslint config file added and app.js error fixing

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const expressSession = require('express-session')({
+  secret: 'random string here are good',
+  resave: false,
+  saveUninitialized: false,
+});
+const User = require('./models/user');
 
 const indexRouter = require('./routes/index');
 const api = require('./routes/api/index');
@@ -24,11 +30,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('express-session')({
-	secret: 'random string here are good',
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(expressSession);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -38,7 +40,6 @@ app.use('/api', api);
 app.use('/api/users', users);
 
 // Configure Passport
-const User = require('./models/user');
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
