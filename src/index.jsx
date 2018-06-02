118680648b4dfe6f23454e8e5f026b80b71e6805
jimyandres <jimyandres@utp.edu.co>
2018-06-01 21:02:38 -0500

    Webpack added, some config files created and a minor test with react added

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import TestComponent from './TestComponent';
+
+render(
+	<TestComponent />,
+	document.querySelector('#react-app')
+);
