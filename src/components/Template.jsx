468fa7c628e07c39a083190baf379aaec2658135
jimyandres <jimyandres@utp.edu.co>
2018-06-02 19:37:32 -0500

    HomePage component added

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import Header from './shared/Header';
+import HomePage from './home/HomePage';
 
 const Template = () => {
   return (
     <div className="wrapper">
       <Header username="anonymous" />
-      <div className="content">
-        Content will eventually go here.
-      </div>
+      <HomePage />
     </div>
   );
 };
---
+++ src/components/home/HomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <section className="page-content">
+      <div className="row">
+        <div className="col-sm-12 col-lg-8">
+          This is the Home Page.
+        </div>
+        <aside className="col-sm-12 col-lg-4">
+          This is the sidebar.
+        </aside>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
