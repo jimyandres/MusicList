530740d234bddbd1b41793c2b45e1bf7b0e942d5
jimyandres <jimyandres@utp.edu.co>
2018-06-02 20:03:18 -0500

    404 fixed when getting the page from the url

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const expressSession = require('express-session')({
-	secret: 'random string here are good',
-	resave: false,
-	saveUninitialized: false,
+  secret: 'random string here are good',
+  resave: false,
+  saveUninitialized: false,
 });
 const User = require('./models/user');
 
@@ -35,9 +35,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api', api);
 app.use('/api/users', users);
+app.use('/*', indexRouter);
 
 // Configure Passport
 passport.use(new LocalStrategy(User.authenticate()));
@@ -46,18 +46,18 @@ passport.deserializeUser(User.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use((err, req, res, next) => {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
