f853fcab8c3d23cd13c9fd04e93a05fc78fb226d
jimyandres <jimyandres@utp.edu.co>
2018-06-16 12:38:35 -0500

    Minor changes: added albums and artists to the User model

--- models/user.js
+++ models/user.js
@@ -4,12 +4,13 @@ const { Schema } = mongoose;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new Schema({
-  username: String,
-  password: { type: String, select: false },
+  albums: [Schema.Types.Mixed],
+  artists: [Schema.Types.Mixed],
+  email: String,
   firstName: String,
   lastName: String,
-  email: String,
   passwordReset: { type: String, select: false },
+  username: String,
 });
 
 User.plugin(passportLocalMongoose);
