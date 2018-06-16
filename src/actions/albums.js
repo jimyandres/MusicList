c44f7439c2b29ce1526fbe6d374ca4668990401f
jimyandres <jimyandres@utp.edu.co>
2018-06-16 11:33:53 -0500

    Album feature integrated into Action creators

--- src/actions/albums.js
+++ src/actions/albums.js
@@ -3,9 +3,52 @@ import { decrementProgress, incrementProgress } from './progress';
 import { clearError } from './error';
 
 // Action Creators
+const albumAddFailure = error => ({ type: 'MUSIC_ALBUM_ADD_FAILURE', error });
+const albumAddSuccess = json => ({ type: 'MUSIC_ALBUM_ADD_SUCCESS', json });
 const albumSearchClear = () => ({ type: 'MUSIC_ALBUM_SEARCH_CLEAR' });
 const albumSearchFailure = error => ({ type: 'MUSIC_ALBUM_SEARCH_FAILURE', error });
 const albumSearchSuccess = json => ({ type: 'MUSIC_ALBUM_SEARCH_SUCCESS', json });
+
+// Add an album
+const addAlbum = (id) => {
+  return async (dispatch) => {
+    // Clear the error box if it is displayed
+    dispatch(clearError());
+
+    // turn on spinner
+    dispatch(incrementProgress());
+
+    // Send packet to our API, wich will communicate with Discogs
+    await fetch(
+      // where to contact
+      '/api/albums/add',
+      // what to send
+      {
+        method: 'POST',
+        body: JSON.stringify(id),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+      },
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    }).then((json) => {
+      if (json.results) {
+        return dispatch(albumAddSuccess(json));
+      }
+      return dispatch(albumAddFailure(new Error(json.error)));
+    }).catch((err) => {
+      dispatch(albumAddFailure(new Error(err)));
+    });
+
+    // turn off spinner
+    return dispatch(decrementProgress());
+  };
+};
 
 // Search Album
 const searchAlbum = (searchText) => {
@@ -55,12 +98,14 @@ const searchAlbum = (searchText) => {
   };
 };
 
-
 export {
   // Action Creators
+  albumAddFailure,
+  albumAddSuccess,
   albumSearchClear,
   albumSearchFailure,
   albumSearchSuccess,
   // Others
+  addAlbum,
   searchAlbum,
 };
