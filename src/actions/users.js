683d4eb9725c5480f92e331916d1e1af36049575
jimyandres <jimyandres@utp.edu.co>
2018-06-18 21:01:26 -0500

    Users action creator added

---
+++ src/actions/users.js
@@ -0,0 +1,54 @@
+import 'whatwg-fetch';
+import { decrementProgress, incrementProgress } from './progress';
+import { clearError } from './error';
+
+// Action Creators
+const userClearList = () => ({ type: 'USER_CLEAR_LIST' });
+const userLookupFailure = error => ({ type: 'USER_LOOKUP_FAILURE', error });
+const userLookupSuccess = json => ({ type: 'USER_LOOKUP_SUCCESS', json });
+
+// Look up a user
+const userLookup = (username) => {
+  return async (dispatch) => {
+    // clear the error box if it's displayed
+    dispatch(clearError());
+
+    // turn on spinner
+    dispatch(incrementProgress());
+
+    // API call
+    await fetch(
+      '/api/users/find',
+      {
+        method: 'POST',
+        body: JSON.stringify({ username }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+      },
+    ).then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return null;
+    }).then((json) => {
+      if (json.username) {
+        return dispatch(userLookupSuccess(json));
+      }
+      return dispatch(userLookupFailure(new Error(json.error)));
+    }).catch(error => dispatch(userLookupFailure(new Error(error))));
+
+    // turn off spinner
+    return dispatch(decrementProgress());
+  };
+};
+
+export {
+  // Action creators
+  userClearList,
+  userLookupFailure,
+  userLookupSuccess,
+  // Others
+  userLookup,
+};
